b22bdfa33d4e7e75449674393dc8e867f3c6720b
Roland Fong <dev.Fong.Roland@dev.bwater.com>
2020-09-14 15:42:22 -0400

    Fix yml file

--- create_pipelines.sh
+++ create_pipelines.sh
@@ -4,7 +4,7 @@ for f in */pipeline-spec.yml ; do
     echo "$f"
     err=$(codefresh create pipeline -f $f 2>&1)
     echo $err
-    if $err == *"already exists"*; then
+    if [[ $err == *"already exists"* ]]; then
       codefresh replace pipeline -f $f
     fi 
 done
