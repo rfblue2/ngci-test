06a14937e13a029d8a58ec1f7bfc6738e4d340ce
Roland Fong <dev.Fong.Roland@dev.bwater.com>
2020-09-15 12:48:01 -0400

    Update pipelines again, project1

--- create_pipelines.sh
+++ create_pipelines.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 for f in */pipeline-spec.yml ; do
-    echo "$f"
     err=$(codefresh create pipeline -f $f 2>&1)
-    echo $err
     if [[ $err == *"already exists"* ]]; then
-      codefresh replace pipeline -f $f
+      codefresh replace pipeline -f $f > /dev/null
     fi 
 done
 
 changed=$(git diff --name-only HEAD HEAD~1)
-echo $changed
 if [[ $changed == "project1/"** ]]; then
-    echo 'set project1'
-    PROJECT1=true
+    export PROJECT1=true
 elif [[ $changed == "project2/"** ]]; then
-    echo 'set project2'
-    PROJECT2=true
+    export PROJECT2=true
 fi
-echo $PROJECT1
-echo $PROJECT2
+cf_export PROJECT1 PROJECT2
 
